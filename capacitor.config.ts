74482a160e8527dd3527be8c3c8f00c7d88f975e
lazmiano-web <lazmiano@gmail.com>
2026-09-25 01:28:12 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cruisecareer.app',
+  appName: 'CruiseCareer',
+  webDir: 'out'
+};
+
+export default config;
